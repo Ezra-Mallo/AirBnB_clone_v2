c4b00742a349de1417adcd96cd864cdf83ac5891
Ezra-Mallo <ezra.mallo@gmail.com>
2023-06-22 11:23:33 +0100

    Updated Task 2

--- models/place.py
+++ models/place.py
@@ -3,8 +3,9 @@
 
 import models
 from models.base_model import BaseModel, Base
-from sqlalchemy import Column, String, Integer, Float
 from sqlalchemy.orm import relationship
+from sqlalchemy import Column, Integer, String, ForeignKey, Float, Table
+
 
 class Place(BaseModel, Base):
     """
@@ -25,16 +26,15 @@ class Place(BaseModel, Base):
                             Amenity.id later
     """
 
-    __tablename__ = "places"
+    __tablename__ = 'places'
 
-    city_id = Column(String(60), nullable=False, ForeignKey("cities.id"))
-    user_id = Column(String(60), nullable=False, ForeignKey("users.id"))
+    city_id = Column(String(60), ForeignKey('cities.id'), nullable=False)
+    user_id = Column(String(60), ForeignKey('users.id'), nullable=False)
     name = Column(String(128), nullable=False)
     description = Column(String(1024), nullable=True)
-    number_rooms = Column(Integer, nullable=False, default=0) 
-    number_bathrooms = Column(Integer, nullable=False, default=0) 
+    number_rooms = Column(Integer, nullable=False, default=0)
+    number_bathrooms = Column(Integer, nullable=False, default=0)
     max_guest = Column(Integer, nullable=False, default=0)
     price_by_night = Column(Integer, nullable=False, default=0)
     latitude = Column(Float, nullable=True)
     longitude = Column(Float, nullable=True)
-    amenity_ids = []
