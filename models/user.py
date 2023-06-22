4d76ea0b3001ec29f14958b4a277104b7fdfdcfd
Ezra-Mallo <ezra.mallo@gmail.com>
2023-06-22 04:07:33 +0100

    Added Task 7. DBStorage - User

--- models/user.py
+++ models/user.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
 """class User is defined here to inherit from BaseModel"""
 import models
-from models.base_model import BaseModel
+from models.base_model import BaseModel, BaseModel
+from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
 
-
-class User(BaseModel):
+class User(BaseModel, Base):
     """class User inherits from BaseModel
     Arguments:
         email = empty string
@@ -14,7 +15,9 @@ class User(BaseModel):
     """
 
     # public instances
-    email = ""
-    password = ""
-    first_name = ""
-    last_name = ""
+    __tablename__ = "users"
+
+    email = Column(String(128), nullable=False)
+    password = Column(String(128), nullable=False)
+    first_name = Column(String(128), nullable=True)
+    last_name =  Column(String(128), nullable=True)
