66291ee926560947edc137967c86d616ef0fab74
Ezra-Mallo <ezra.mallo@gmail.com>
2023-07-06 20:23:33 +0100

    Updated task 0. Prepare your web servers

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -6,22 +6,27 @@ sudo apt install nginx -y
 # create the folders and path if it does not exist
 sudo mkdir -p /data/web_static/releases/test/
 sudo mkdir -p /data/web_static/shared/
-
 # create file with content if not exist
-content="<!DOCTYPE html><html><body><h1>This is my index.html file. Release 1.0 </h1></body></html>"
-echo "$content" | sudo tee /data/web_static/releases/test/index.html
-sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
+echo echo "<!DOCTYPE html>
+<html>
+  <head>
+  </head>
+  <body>
+    <p>Nginx server test</p>
+  </body>
+</html>" | sudo tee /data/web_static/releases/test/index.html
 
 # change user:group ownership of /data/
+sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
 sudo chown -R ubuntu:ubuntu /data
 
 # create symbolic link for current if not exist
 alias_config=\
 "   location /hbnb_static {
-	alias /data/web_static/current/;
+	alias /data/web_static/current;
 }"
-sudo echo -e "$alias_config" | sudo tee -a /etc/nginx/sites-available/default
+# sudo echo -e "$alias_config" | sudo tee -a /etc/nginx/sites-available/default
+sudo sed -i '39 i\ \tlocation /hbnb_static {\n\t\talias /data/web_static/current;\n\t}\n' /etc/nginx/sites-available/default
 
-sudo service nginx restart
-#sudo sed -i '39 i\ \tlocation /hbnb_static {\n\t\talias /data/web_static/current;\n\t}\n' /etc/nginx/sites-enabled/default
-#sudo service nginx restart
+
+sudo service nginx start
