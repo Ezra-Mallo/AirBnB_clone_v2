9aa1ea9d255302decb6f0879c69e4aaaf3dd0340
Ezra-Mallo <ezra.mallo@gmail.com>
2023-07-18 17:29:32 +0100

    More updates on task 1 & 2

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 """ Fabfile to distribute an archive to a web server."""
-
 import os.path
 from fabric.api import env
 from fabric.api import put
@@ -11,6 +10,7 @@ env.hosts = ['18.206.197.169', '100.25.183.151']
 
 def do_deploy(archive_path):
     """Distributes an archive to a web server.
+
     Args:
         archive_path (str): The path of the archive to distribute.
     Returns:
