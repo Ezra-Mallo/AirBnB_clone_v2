4679d0f7b6dcbdb6ae441099e2bdfab631603288
Ezra-Mallo <ezra.mallo@gmail.com>
2023-07-24 01:45:45 +0100

    Added task 2. C is fun!

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -9,14 +9,23 @@ app = Flask(__name__)
 
 
 @app.route('/', strict_slashes=False)
-def hello():
-    """Return a given string"""
-    return ("Hello HBNB!")
+def hello_hbnb():
+    """displays "Hello HBNB!" for route: /"""
+    return ('Hello HBNB!')
+
 
 @app.route('/hbnb', strict_slashes=False)
-def HBNB():
-    """Return a given string"""
-    return ("HBNB!")
+def hello():
+    """displays "HBNB" for route: /hbnb"""
+    return ('HBNB')
 
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000, debug=None)
+@app.route("/c/<text>", strict_slashes=False)
+def cText(text):
+    """displays /c/<text>: display “C ” followed by the value of the text
+    variable (replace underscore _ symbols with a space )"""
+    return "C {}".format(text.replace("_", " "))
+
+
+if __name__ == '__main__':
+    ''' main function '''
+    app.run(host='0.0.0.0', port=5000)
