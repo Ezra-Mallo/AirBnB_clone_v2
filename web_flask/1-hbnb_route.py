f7a1ef2f527ebee9257e41928227bc6730708721
Ezra-Mallo <ezra.mallo@gmail.com>
2023-07-24 01:32:05 +0100

    Updated Task 1. HBNB

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -9,16 +9,21 @@ app = Flask(__name__)
 
 
 @app.route('/', strict_slashes=False)
-def hello():
-    """Return a given string"""
-    return ("Hello HBNB!")
+def hello_hbnb():
+    '''
+    displays "Hello HBNB!" for route: /
+    '''
+    return ('Hello HBNB!')
 
 
 @app.route('/hbnb', strict_slashes=False)
-def hbnb():
-    """Return a given string"""
-    return ("HBNB!")
+def hello():
+    '''
+    displays "HBNB" for route: /hbnb
+    '''
+    return ('HBNB')
 
 
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000, debug=None)
+if __name__ == '__main__':
+    ''' main function '''
+    app.run(host='0.0.0.0', port=5000)
