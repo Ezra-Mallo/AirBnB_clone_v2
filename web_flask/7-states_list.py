6eb631d83983b4f08863547aeb2a1a8b0b0b8dde
Ezra-Mallo <ezra.mallo@gmail.com>
2023-07-25 10:26:24 +0100

    Added task 10 and 11

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,73 +1,26 @@
 #!/usr/bin/python3
-''' starts a Flask web application '''
+"""Importing Flask to run the web app"""
+
 from flask import Flask, render_template
+from models import storage
+from models.state import State
+
 
 app = Flask(__name__)
 
 
-@app.route('/', strict_slashes=False)
-def hello_hbnb():
-    '''
-    displays "Hello HBNB!" for route: /
-    '''
-    return 'Hello HBNB!'
+@app.route("/states_list", strict_slashes=False)
+def display_states():
+    """Render state_list html page to display States created"""
+    states = storage.all()
+    return render_template('7-states_list.html', states=states)
 
 
-@app.route('/hbnb', strict_slashes=False)
-def hello():
-    '''
-    displays "HBNB" for route: /hbnb
-    '''
-    return 'HBNB'
+@app.teardown_appcontext
+def teardown(self):
+    """Method to remove current SQLAlchemy Session"""
+    storage.close()
 
 
-@app.route('/c/<text>', strict_slashes=False)
-def c_is_fun(text):
-    '''
-    - displays "C" followed by value of the text variable
-    - replaces underscore "_" with a space " "
-    '''
-    return 'C ' + text.replace('_', ' ')
-
-
-@app.route('/python/', strict_slashes=False)
-@app.route('/python/<text>', strict_slashes=False)
-def python_is_cool(text='is_cool'):
-    '''
-    - displays "Python" followed by the value of the text
-    - replaces underscore "_" with a space " "
-    - displays default value if route is /python/
-    '''
-    return 'Python ' + text.replace('_', ' ')
-
-
-@app.route('/number/<int:n>', strict_slashes=False)
-def display_number(n):
-    '''
-    - displays "<n> is a number" only if n is an integer
-    '''
-    return '{} is a number'.format(n)
-
-
-@app.route('/number_template/<int:n>', strict_slashes=False)
-def number_template(n):
-    '''
-    - displays a HTML page only if n is an integer
-    format:
-        - "Number: n" inside body tag
-    '''
-    return render_template('5-number.html', value=n)
-
-
-@app.route('/number_odd_or_even/<int:n>', strict_slashes=False)
-def number_odd_or_even(n):
-    """displays a HTML page only if n is an integer
-    format:
-        - H1 tag: “Number: n is even|odd” inside the tag BODY
-    """
-    return render_template('6-number_odd_or_even.html', value=n)
-
-
-if __name__ == '__main__':
-    ''' main function '''
+if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
